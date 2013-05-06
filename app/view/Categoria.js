ea53400ed24937007e409b9765c2371320746823
Alberto Dal Dosso <daldosso@gmail.com>
2013-05-06 09:22:30 +0200

    Varie

---
+++ app/view/Categoria.js
@@ -0,0 +1,32 @@
+Ext.define('QuantoSpendi.view.Categoria', {
+    extend: 'Ext.form.Panel',
+    xtype: 'categoria',
+    config: {
+        url: 'srv/categoria-add.php',
+        title: 'Aggiungi Categoria',
+        items: [{
+                xtype: 'toolbar',
+                docked: 'top',
+                items: [{
+                        xtype: 'spacer'
+                    }, {
+                        text: 'Conferma',
+                        action: 'confirm'
+                    }]
+            }, {
+                xtype: 'fieldset',
+                defaults: {
+                    labelWidth: '35%'
+                },
+                items: [{
+                        xtype: 'textfield',
+                        name: 'descrizione',
+                        label: 'Descrizione',
+                        clearIcon: true
+                    }
+                ]}]
+
+
+    }
+
+});
